dba4ed779ce8c0f034ed37345bc1de990c054b54
Garima16 <gjain699@gmail.com>
2020-11-19 11:37:01 +0530

    Add Discriminator model

--- dcgan.py
+++ dcgan.py
@@ -56,3 +56,53 @@ class Generator(nn.Module):
         output = torch.tanh(input=self.convt5(layer4_out))
 
         return output
+
+
+class Discriminator(nn.Module):
+
+    def __init__(self, nc, ndf):
+        super(Discriminator, self).__init__()
+
+        # input to Discriminator layer 1 = (nc, 64x64), inplace=True ; ndf=64, ngf=64
+        self.conv1 = nn.Conv2d(in_channels=nc, out_channels=ndf, kernel_size=4,
+                               stride=2, padding=1, bias=False)
+
+        # input to Discriminator layer 2 = (ndf, 32x32)
+        self.conv2 = nn.Conv2d(in_channels=ndf, out_channels=ndf * 2, kernel_size=4,
+                               stride=2, padding=1, bias=False)
+        self.batch_norm1 = nn.BatchNorm2d(ndf * 2)
+
+        # input to Discriminator layer 3 = (ndf * 2, 16x16)
+        self.conv3 = nn.Conv2d(in_channels=ndf * 2, out_channels=ndf * 4, kernel_size=4,
+                               stride=2, padding=1, bias=False)
+        self.batch_norm2 = nn.BatchNorm2d(ndf * 4)
+
+        # input to Discriminator layer 4 = (ndf * 4, 8x8)
+        self.conv4 = nn.Conv2d(in_channels=ndf * 4, out_channels=ndf * 8, kernel_size=4,
+                               stride=2, padding=1, bias=False)
+        self.batch_norm3 = nn.BatchNorm2d(ndf * 8)
+
+        # input to Discriminator layer 5 = (ndf * 8, 4x4)
+        self.conv5 = nn.Conv2d(in_channels=ndf * 8, out_channels=1, kernel_size=4,
+                               stride=1, padding=0, bias=False)
+
+    def forward(self, x):
+        layer1_out = F.leaky_relu(input=self.conv1(x),
+                                  negative_slope=0.2,
+                                  inplace=True)
+
+        layer2_out = F.leaky_relu(input=self.batch_norm1(self.conv2(layer1_out)),
+                                  negative_slope=0.2,
+                                  inplace=True)
+
+        layer3_out = F.leaky_relu(input=self.batch_norm2(self.conv3(layer2_out)),
+                                  negative_slope=0.2,
+                                  inplace=True)
+
+        layer4_out = F.leaky_relu(input=self.batch_norm3(self.conv4(layer3_out)),
+                                  negative_slope=0.2,
+                                  inplace=True)
+
+        output = torch.sigmoid(input=self.conv5(layer4_out))
+
+        return output
